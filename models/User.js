baad4579906516e4afbb44ef347249bcd7d439ae
Upfue <120177504+upfue@users.noreply.github.com>
2023-03-24 15:33:56 +0800

    hash password done

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,6 +30,11 @@ const UserSchema = new mongoose.Schema({
     trim: true,
     default: "lastname",
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 export default mongoose.model("User", UserSchema);
